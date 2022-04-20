8a75c9f1ed5a88b85c83caae0b403a400b31c4e4
natsumerinchan <jm1ts5568625@yahoo.com>
2022-04-20 08:53:32 +0000

    update gearlock/gearboot/boot-comp.sh.

--- gearlock/gearboot/boot-comp.sh
+++ gearlock/gearboot/boot-comp.sh
@@ -4,4 +4,19 @@
 # You must delete this if you dont need it.
 # Note: You don't need to manually deal with this file, GearLock will take care of it.
 
-enable_nativebridge
+if test -d "$SYSTEM_DIR/lib64/hw"; then
+	SYSTEM_ARCH=x86_64
+else
+	SYSTEM_ARCH=x86
+fi
+
+if test "$SYSTEM_ARCH" != "x86_64"; then
+	enable_nativebridge
+else
+    if test "$sdk" -ge "26"; then
+        enable_nativebridge
+    else
+        enable_nativebridge
+        enable_nativebridge 64
+    fi  
+fi
