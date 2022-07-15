7edfa8bffd3513ce46edd7a28c01fa8e44273e55
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-15 19:34:59 +0800

    Create Android9 Branch

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 get_base_dir # Returns execution directory path in $BD variable
 # get_net_stat
 check_compat 7.2.22
-HOUDINI_API=26
+HOUDINI_API=28
 #####--- Import Functions ---#####
 
 # Ensure compatible GearLock version
