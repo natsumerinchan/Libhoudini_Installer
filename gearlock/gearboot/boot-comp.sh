32959e224f20351eade106002fc84de8f333e5eb
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-16 15:30:18 +0800

    Update boot-comp.sh

--- gearlock/gearboot/boot-comp.sh
+++ gearlock/gearboot/boot-comp.sh
@@ -4,4 +4,6 @@
 # You must delete this if you dont need it.
 # Note: You don't need to manually deal with this file, GearLock will take care of it.
 
+mount -o remount,rw /
+mount -o remount,rw /system
 enable_nativebridge
